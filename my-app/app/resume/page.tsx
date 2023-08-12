eafd5829546fb316af9ca1db7bce68c5b6c13862
Diwanshu <saidiwanshu1880@gmail.com>
2023-08-12 14:48:56 +0530

    First Commit

---
+++ my-app/app/resume/page.tsx
@@ -0,0 +1,17 @@
+import ResumePDF from "@/Components/Resume/Resume";
+import { PDFViewer } from "@react-pdf/renderer";
+
+const Resume = () => {
+  return (
+    <section className=" pt-16 min-h-screen">
+      <div className=" container px-11  text-white mx-auto">
+        <h2 className=" text-center my-5 text-md sm:text-lg">
+          Checkout My resume
+        </h2>
+          <ResumePDF />
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
